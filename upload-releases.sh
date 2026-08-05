b14c7def929cff460193baf106dd20a4fdf71ebc
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-08-05 15:51:54 +0300

    Move uploaded releases to a separate directory

--- upload-releases.sh
+++ upload-releases.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir -p releases-uploaded
 cd releases
 
 for f in * ; do
   echo $f
   gh release view $f >/dev/null 2>&1 || gh release create $f -n $f || exit 1
   gh release upload --clobber $f $f/*.deb || exit 1
+  mv $f ../releases-uploaded/ || exit 1
 done
