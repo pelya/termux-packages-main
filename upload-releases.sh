bd9347c9c0998c692bf0372abbbce57a69befd94
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-08-05 16:39:47 +0300

    Added upload rate limiting so Github won't complain

--- upload-releases.sh
+++ upload-releases.sh
@@ -2,10 +2,17 @@
 
 mkdir -p releases-uploaded
 cd releases
+COUNT=0
 
 for f in * ; do
   echo $f
   gh release view $f >/dev/null 2>&1 || gh release create $f -n $f || exit 1
   gh release upload --clobber $f $f/*.deb || exit 1
   mv $f ../releases-uploaded/ || exit 1
+  let COUNT++
+  if [ $COUNT -ge 40 ]; then
+    echo "Sleeping 1 hour because of Github rate limits"
+    COUNT=0
+    sleep 3600
+  fi
 done
