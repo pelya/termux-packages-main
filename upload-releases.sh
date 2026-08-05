05bb26d118089bcc5689a847bc5f911b35bfc06b
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-08-05 14:05:24 +0300

    Added exit on error

--- upload-releases.sh
+++ upload-releases.sh
@@ -4,6 +4,6 @@ cd releases
 
 for f in * ; do
   echo $f
-  gh release view $f >/dev/null 2>&1 || gh release create $f -n $f
-  gh release upload --clobber $f $f/*.deb
+  gh release view $f >/dev/null 2>&1 || gh release create $f -n $f || exit 1
+  gh release upload --clobber $f $f/*.deb || exit 1
 done
