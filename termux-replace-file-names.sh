f167022c4a44e2627038837f7fecb69367bffa6e
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-08-06 09:28:50 +0300

    Updated filenames in all packages to replace all special symbols with dots

--- termux-replace-file-names.sh
+++ termux-replace-file-names.sh
@@ -5,3 +5,12 @@ for f in dists/*/*/*/Packages; do
   sed -i -E '/^Filename: / s/[^-_.a-zA-Z0-9/ ]/./g' "$f"
   sed -i -E 's/^Filename. /Filename: /g' "$f"
 done
+
+../termux-apt-repo/termux-apt-repo --sign --use-hard-links --github-releases --no-contents-xz --add-existing-repo \
+  debs-main . stable main
+
+../termux-apt-repo/termux-apt-repo --sign --use-hard-links --github-releases --no-contents-xz --add-existing-repo \
+  debs-x11 . x11 main
+
+../termux-apt-repo/termux-apt-repo --sign --use-hard-links --github-releases --no-contents-xz --add-existing-repo \
+  debs-root . root stable
