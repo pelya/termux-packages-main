9bb044e6339dbb549102ba117e440a6d53a72ea9
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-08-06 10:04:15 +0300

    Replace pluses in filenames

--- termux-replace-file-names.sh
+++ termux-replace-file-names.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+find releases-uploaded -name '*+*' -printf "%f\n" | sort -u > files-pluses.txt
+
+cat files-pluses.txt | while read FNAME; do
+  OLDNAME=$(echo "${FNAME}" | sed -E 's/[^-_.a-zA-Z0-9]/./g')
+  echo "${OLDNAME} -> ${FNAME}"
+  for f in dists/*/*/*/Packages; do
+    echo "$f"
+    sed -i -E "s/${OLDNAME}/${FNAME}/g" "$f"
+  done
+
+done
+
 for f in dists/*/*/*/Packages; do
   echo "$f"
-  sed -i -E '/^Filename: / s/[^-_.a-zA-Z0-9/ ]/./g' "$f"
+  sed -i -E '/^Filename: / s/[^-_.a-zA-Z0-9+ /]/./g' "$f"
   sed -i -E 's/^Filename. /Filename: /g' "$f"
 done
 
